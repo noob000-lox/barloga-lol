1feeb3f282b1df87709f349a2c2a3946ef63d90a
Yurii <palyura43@gmail.com>
2024-09-13 15:11:49 +0200

    Зірка

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,11 @@
-print("Hello Andiy")
+import math
+d = 5 
+def odd(d):
+    rows = math.ceil(d/2)
+    for spaces in range(rows-1,-1,-1):
+        star = d - (spaces * 2)
+        print(" " * spaces + "*" * star)
+    for spaces in range(1,rows):
+        star = d - (spaces * 2)
+        print(" " * spaces + "*" * star)
+odd(25)
